0a3d11970bd69f065f1b0432b951b020a29eba76
Arian Pieroni <rizelloyd@gmail.com>
2025-07-29 21:06:38 +0200

    extracted password generator to seperate class

---
+++ src/main/java/org/R4ZXRN3T/PasswordGenerator.java
@@ -0,0 +1,105 @@
+package org.R4ZXRN3T;
+
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+
+import javax.swing.*;
+
+public class PasswordGenerator {
+
+	private static JDialog frame = new JDialog(Main.frame, "Password Generator", true);
+
+	public static void showPasswordGeneratorDialog() {
+		if (frame.isVisible()) {
+			frame.requestFocus();
+			return;
+		}
+		frame = new JDialog(Main.frame, "Password Generator", true);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setLayout(new BorderLayout(10, 10));
+		frame.setIconImage(Icons.FIRSTPASS_ICON.getImage());
+		frame.setSize(450, 250);
+		frame.setLocationRelativeTo(Main.frame);
+		frame.setResizable(false);
+
+		JPanel optionsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		optionsPanel.setPreferredSize(new Dimension(500, 85));
+		optionsPanel.setBorder(BorderFactory.createTitledBorder("Options:"));
+		JCheckBox uppercase = new JCheckBox("Uppercase");
+		JCheckBox lowercase = new JCheckBox("Lowercase");
+		JCheckBox numbers = new JCheckBox("Numbers");
+		JCheckBox specialCharacters = new JCheckBox("Special Characters");
+		JSlider lengthSlider = new JSlider(1, 64, 16);
+		lengthSlider.setBackground(Color.LIGHT_GRAY);
+		JLabel lengthLabel = new JLabel("Length:");
+		JLabel lengthValue = new JLabel("16");
+		lengthSlider.addChangeListener(e -> lengthValue.setText(String.valueOf(lengthSlider.getValue())));
+
+		uppercase.setSelected(true);
+		lowercase.setSelected(true);
+		numbers.setSelected(true);
+		specialCharacters.setSelected(true);
+
+		optionsPanel.add(uppercase);
+		optionsPanel.add(lowercase);
+		optionsPanel.add(numbers);
+		optionsPanel.add(specialCharacters);
+		optionsPanel.add(lengthSlider);
+		optionsPanel.add(lengthLabel);
+		optionsPanel.add(lengthValue);
+
+		JPanel outputPanel = new JPanel(new BorderLayout(4, 4));
+		outputPanel.setBorder(BorderFactory.createTitledBorder("Generated Password:"));
+
+		JTextField passwordField = new JTextField();
+		passwordField.setEditable(false);
+		passwordField.setBorder(BorderFactory.createEmptyBorder(4, 8, 4, 4));
+
+
+		JScrollPane scrollPane = new JScrollPane(passwordField);
+		scrollPane.setAutoscrolls(true);
+		scrollPane.setBackground(new Color(0, 0, 0, 0));
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+		JButton copyButton = getCopyButton(passwordField);
+
+		outputPanel.add(scrollPane, BorderLayout.CENTER);
+		outputPanel.add(copyButton, BorderLayout.EAST);
+
+		CustomButton generateButton = new CustomButton("Generate!",
+				e -> passwordField.setText(generatePassword(lengthSlider.getValue(), uppercase.isSelected(), lowercase.isSelected(), numbers.isSelected(), specialCharacters.isSelected())),
+				new Dimension(100, 35));
+		generateButton.setToolTipText("Generate a new password");
+		generateButton.setBackground(UIManager.getColor("Button.background").brighter());
+
+		JPanel generatePanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		generatePanel.add(generateButton);
+
+		frame.add(optionsPanel, BorderLayout.NORTH);
+		frame.add(outputPanel, BorderLayout.CENTER);
+		frame.add(generatePanel, BorderLayout.SOUTH);
+
+		frame.setVisible(true);
+	}
+
+	private static JButton getCopyButton(JTextField passwordField) {
+		JButton copyButton = new JButton();
+		copyButton.setIcon(Main.darkMode ? Icons.COPY_ICON_WHITE_SCALED : Icons.COPY_ICON_SCALED);
+		copyButton.setToolTipText("Copy password to clipboard");
+		copyButton.addActionListener(e -> {
+			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(passwordField.getText()), null);
+			JOptionPane.showMessageDialog(frame, "Password copied to clipboard", "Success", JOptionPane.INFORMATION_MESSAGE);
+		});
+		copyButton.setPreferredSize(new Dimension(40, 40));
+		return copyButton;
+	}
+
+	private static String generatePassword(int length, boolean uppercase, boolean lowercase, boolean numbers, boolean specialCharacters) {
+		String characterSet = "";
+		if (uppercase) characterSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		if (lowercase) characterSet += "abcdefghijklmnopqrstuvwxyz";
+		if (numbers) characterSet += "0123456789";
+		if (specialCharacters) characterSet += "!@#$%^&*()-_=+[{]};:'<A,<.>/?";
+		return Tools.generateRandomString(length, characterSet);
+	}
+}
