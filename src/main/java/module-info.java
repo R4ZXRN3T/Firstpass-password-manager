20d6386a31f152c4fba55de69756eacfdb2d5298
Arian Pieroni <rizelloyd@gmail.com>
2025-11-27 19:32:44 +0100

    Added jre creation with jlink

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.R4ZXRN3T.Firstpass {
+	// JDK modules
+	requires java.base;
+	requires java.desktop;  // Swing, AWT
+
+	// Third-party libraries
+	requires org.json;
+	requires com.formdev.flatlaf;
+	requires jasypt;
+
+	// Export your main package (contains Main, Firstpass, etc.)
+	exports org.R4ZXRN3T;
+}
